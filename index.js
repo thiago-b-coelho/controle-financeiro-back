eaf56460562f026b66c85d3ffcea3791a1385fa6
ThiagobCoelho <tbarroscoelho@gmail.com>
2023-11-20 22:44:51 -0300

    fix: change scripts and port

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ app.use(express.json())
 app.get('/health', (_, res) => {
   return res.send('sistema operacional!');
 });
+
+const PORT = 8080
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
+})
