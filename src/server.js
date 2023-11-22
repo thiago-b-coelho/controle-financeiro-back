7a89fea933c42e12a5fe5a6677c1da7c811badeb
ThiagobCoelho <tbarroscoelho@gmail.com>
2023-11-21 22:01:13 -0300

    chore: add cors

--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
 const express = require("express");
+const cors = require('cors');
 const userRouter = require('./modules/user/user.route.js');
-const authRouter = require('./modules/auth/auth.route.js')
+const authRouter = require('./modules/auth/auth.route.js');
 const app = express();
-app.use(express.json())
+
+app.use(express.json());
+app.use(cors());
 
 app.use('/user', userRouter);
-app.use('/auth', authRouter)
+app.use('/auth', authRouter);
 
 app.get('/health', (_, res) => {
   return res.send('sistema operacional!');
 });
 
-const PORT = 8080
+const PORT = 8080;
+
 app.listen(PORT, async () => {
   console.log(`Servidor rodando na porta ${PORT}`)
 });
