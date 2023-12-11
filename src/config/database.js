51b22b424bf28f472ae846ca79c6e2941cd8ee18
ThiagobCoelho <tbarroscoelho@gmail.com>
2023-12-11 20:42:22 -0300

    change: change from dev to production

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,27 @@
 const dotenv = require('dotenv');
 const env = dotenv.config().parsed;
 
+
 module.exports = {
-  development: {
-    client: 'mysql2',
-    connection: {
-      host: env.DB_HOST || '127.0.0.1',
-      port: 3306,
-      user: env.DB_USER || 'user',
-      password: env.DB_PASSWORD || 'password',
-      database: env.DB_NAME || 'db'
-    }
-  },
   production: {
     client: 'mysql2',
-    connection:  process.env.DATABASE_URL
-    }
+    connection: process.env.DATABASE_URL
+  }
 };
+
+// module.exports = {
+//   development: {
+//     client: 'mysql2',
+//     connection: {
+//       host: env.DB_HOST || '127.0.0.1',
+//       port: 3306,
+//       user: env.DB_USER || 'user',
+//       password: env.DB_PASSWORD || 'password',
+//       database: env.DB_NAME || 'db'
+//     }
+//   },
+//   production: {
+//     client: 'mysql2',
+//     connection:  process.env.DATABASE_URL
+//     }
+// };
