c4295bf8d4a787264aaeeb7afcac0c907a9e7c4a
ThiagobCoelho <tbarroscoelho@gmail.com>
2023-12-13 22:07:35 -0300

    chore: add environment

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
 const environment = process.env.NODE_ENV || 'development'
-const config = require("./src/config/database")['production'];
+const config = require("./src/config/database")[environment];
 
 module.exports = config;
